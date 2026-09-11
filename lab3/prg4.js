4c316b84d2697c6ca971f7032459f7dfff9b8972
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-09-11 09:43:55 +0530

    prg4.js

---
+++ lab3/prg4.js
@@ -0,0 +1,31 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "conttent-type": "apllication/json" });
+  console.log("Client URL:", req.url);
+
+  const product = [
+    {
+      name: "Iphone",
+      price: 85000,
+      qty: 2,
+      discount: 15,
+    },
+    {
+      name: "laptop",
+      price: 155000,
+      qty: 1,
+      discount: 25,
+    },
+    {
+        name: "speaker",
+        price: 14999,
+        qty : 2,
+        discount : 15,
+    },
+  ];
+
+  res.end(JSON.stringify(product));
+});
+
+server.listen(3000, () => console.log("prg4 is runnig at 3000...."));
