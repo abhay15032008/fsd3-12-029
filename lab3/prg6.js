d61d1ccf86d9612f3bd796b82d870b4f720e9708
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-09-12 23:52:51 +0530

    prg6.js

---
+++ lab3/prg6.js
@@ -0,0 +1,22 @@
+
+import { createReadStream } from "fs";
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end(`
+      <h1>Home Page</h1>
+      <a href="/product">Product</a>
+      <a href="/contact">Contact Us</a>
+    `);
+  } else if (req.url === "/product") {
+    createReadStream("product.html", { encoding: "utf-8" }).pipe(res);
+  } else if (req.url === "/contact") {
+    createReadStream("contactUs.html", { encoding: "utf-8" }).pipe(res);
+  } else {
+    res.statusCode = 404;
+    res.end("<h1>404 - Page Not Found</h1>");
+  }
+});
+
+server.listen(3000, () => console.log("prg6 is running..."));
