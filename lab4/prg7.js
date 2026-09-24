c026304e91009ea02657b8193559c45c1a8c7fb0
Abhay Singh <abhay.25b01011377@abes.ac.in>
2026-09-24 12:24:54 +0530

    lab4 completed

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,10 +1,10 @@
 import http from "http";
-import { addUsers, getUsers } from "./users.js";
+import { getAllUsers, getUsersById, updateUsers, addUsers, deleteUser} from "./users.js";
 
 const server = http.createServer((req, res) => {
 
   if (req.url === "/api/users" && req.method === "GET") {
-    res.end(JSON.stringify(getUsers()));
+    res.end(JSON.stringify(getAllUsers()));
   } else if (req.url === "/api/users" && req.method === "POST") {
     let body ='';
     req.on('data',(chunk)=>{
@@ -16,18 +16,24 @@ const server = http.createServer((req, res) => {
        res.end(JSON.stringify({ msg: "user added",userCreated }));
     });
    
-  } else if (req.url === "/api/users/1" && req.method == "GET") {
-
-    res.end(JSON.stringify({ msg: "single user with id 1" }));
+  } else if (req.url.startsWith( "/api/users/") && req.method == "GET") {
+   const userId =Number(req.url.split('/').pop())
+    const userFound = getUsersById(userId);
+    if(!userFound){
+      req.end(JSON.stringify({msg: `user not found`}));
+    }else{
+      req.end(JSON.stringify(userFound));
+    }
+    res.end(JSON.stringify({ msg: `Showing detail of user with id  ${userId
+        
+    }` }));
   } else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
   } else if (req.url === "/api/users/1" && req.method === "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
   } else {
-
     res.statusCode = 404;
     res.end();
-    
   }
 });
 server.listen(3000, () => console.log("prg7 is running..."));
